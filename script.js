5a7000bdae6cea4c9ab284e685cf16d852d3acc1
Gustavo de León <alfonso.gus.deleon@gmail.com>
2019-07-25 00:15:40 -0500

    Agregado CCS y primer version del script

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-function cambio() {}
+function ckChange(ckType){
+    if(!Fisica_I_y_Lab.checked){
+        Fisica_II_y_lab.disabled = true;
+    }else {
+        Fisica_II_y_lab.disabled = false;
+    }
+    if (!Fisica_II_y_lab.checked) {
+        Materia3.disabled = true;
+    }else {
+        Materia3.disabled = false;
+    }
+    /*
+    var ckName = document.getElementsByName(ckType.name);
+    var checked = document.getElementById(ckType.id);
+
+    if (checked.checked) {
+      **for(var i=0; i < ckName.length; i++){
+
+          if(!ckName[i].checked){
+              ckName[i].disabled = true;
+          }else{
+              ckName[i].disabled = false;
+          }
+      }
+    }
+    else {
+      for(var i=0; i < ckName.length; i++){
+        ckName[i].disabled = false;
+      }
+  }
+  */
+}
