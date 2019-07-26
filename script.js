a6e73187424dfaa69b9bb401a21d0e244bca466d
Gustavo de León <alfonso.gus.deleon@gmail.com>
2019-07-25 22:17:31 -0500

    Merging Dev in Master (#1)
    * Primer prueba
    
    * Agregado archivo de Script y rellenado de la tabla
    
    * Agregado CCS y primer version del script
    
    * Pruebas con colores en texto

---
+++ script.js
@@ -0,0 +1,35 @@
+function ckChange(ckType){
+    if(!Fisica_I_y_Lab.checked){
+        Fisica_II_y_lab.disabled = true;
+        Fisica_II_y_lab.checked = false;
+        document.getElementById("1-1").style.color = "#ff0000";
+    }else {
+        Fisica_II_y_lab.disabled = false;
+        document.getElementById("1-1").style.color = "#498b2a";
+    }
+    if (!Fisica_II_y_lab.checked) {
+        Materia3.disabled = true;
+    }else {
+        Materia3.disabled = false;
+    }
+    /*
+    var ckName = document.getElementsByName(ckType.name);
+    var checked = document.getElementById(ckType.id);
+
+    if (checked.checked) {
+      **for(var i=0; i < ckName.length; i++){
+
+          if(!ckName[i].checked){
+              ckName[i].disabled = true;
+          }else{
+              ckName[i].disabled = false;
+          }
+      }
+    }
+    else {
+      for(var i=0; i < ckName.length; i++){
+        ckName[i].disabled = false;
+      }
+  }
+  */
+}
